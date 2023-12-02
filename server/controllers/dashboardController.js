304c4baa6ff94ab470fac1560f9f8e33a6183d5e
Rishabh <rarishabhjain@gmail.com>
2023-12-02 16:35:58 +0530

    Added notes to the dashboard and implemented pagination

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -1,11 +1,52 @@
+const Note = require('../models/Notes');
+const mongoose = require('mongoose');
+
 //Get dashboard
 exports.dashboard = async (req, res) => {
+    let perPage = 12;
+    let page = req.query.page || 1;
+
     const locals = {
         title: "Dashboard",
         description: "This is a notes app build using Node.jsm Express.js, MongoDb, Passport!"
     }
-    res.render('dashboard/index', {
-        locals,
-        layout: '../views/layouts/dashboard'
-    });
+
+    try {
+        const notes = await Note.aggregate([
+            {
+                $sort: { createAt: -1 }
+            },
+            {
+                $match: {user: new mongoose.Types.ObjectId(req.user.id)}
+            },
+            {
+                $project: {
+                    title: { $substr: ['$title', 0, 30]},
+                    body: { 
+                        $cond: {
+                            if: { $gte: [{ $strLenCP: "$body" }, 100] },
+                            then: { $concat: [{ $substrCP: ["$body", 0, 100] }, "..."] },
+                            else: "$body"
+                        }
+                    },
+                }
+            }
+        ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Note.countDocuments({ user: new mongoose.Types.ObjectId(req.user.id) });
+
+        res.render('dashboard/index', {
+            userName: req.user.firstName,
+            locals,
+            notes,
+            layout: '../views/layouts/dashboard',
+            current: page,
+            pages: Math.ceil(count / perPage)
+        });
+    } catch(err) {
+        console.log("Error: " + err);
+    }
 }
